93b0954ef4123fbdcf806048d4230282606ac007
slavatarasevich <blackgrif@gmail.com>
2022-10-31 15:48:10 +0300

    Task_2 main functionality is not ready yet

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 "use strict";
+
+let div = document.querySelector(".div1");
+let divContent = document.querySelector(".div1-content");
+div.addEventListener("click", collapse());
+let div2 = document.querySelector(".div2");
+let divContent2 = document.querySelector(".div1-content");
+div2.addEventListener("click", collapse());
+let div3 = document.querySelector(".div3");
+let divContent3 = document.querySelector(".div1-content");
+div3.addEventListener("click", collapse());
+
+function collapse() {
+  let isClosed = true;
+
+  return function () {
+    if (isClosed === true) {
+      divContent.style.transition = "2sec";
+      divContent.style.display = "flex";
+      isClosed = false;
+      console.log("set to false");
+    } else if (isClosed === false) {
+      divContent.style.display = "none";
+      isClosed = true;
+      console.log("set to true");
+    }
+  };
+}
