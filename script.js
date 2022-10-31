0cf4dcd601164167555b4ec9cfb86d4785570296
slavatarasevich <blackgrif@gmail.com>
2022-10-31 13:38:13 +0300

    Task_1 beta version for testing
    main functionality was achieved, needs testing

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 "use strict";
 
 let circle = document.querySelector(".circle");
-circle.addEventListener("mouseover", circleEscape);
+circle.addEventListener("mouseover", circleEscape());
 
-function circleEscape(e) {
-  console.log("hover");
+function initialPosition() {
+  circle.style.left = screen.width / 2 - 50 + "px";
+  circle.style.top = screen.height / 2 - 130 + "px";
+}
+
+initialPosition();
+
+function circleEscape() {
+  console.log("hover", screen.width, screen.height);
+  let circleTop = "";
+  let circleLeft = "";
+  return function () {
+    circle.style.transitionDuration = "2s";
+    circle.style.top = Math.random() * (screen.height - 0) + 0 + "px";
+    circle.style.left = Math.random() * (screen.width - 0) + 0 + "px";
+    console.log("did it move?");
+  };
 }
