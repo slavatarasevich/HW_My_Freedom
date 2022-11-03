b2b9b3c200e214a0cc4be945f29085260570b06c
slavatarasevich <blackgrif@gmail.com>
2022-11-03 17:20:54 +0300

    passed [name]bundle

--- src/analytics.js
+++ src/analytics.js
@@ -19,3 +19,5 @@ function createAnalytics() {
     },
   };
 }
+
+window.analytics = createAnalytics();
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/index.js",
+    analytics: "./src/analytics.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
